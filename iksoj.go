9733db7e6bde7fca525b6a248086add2e8edb5be
Joop Kiefte <joop@kiefte.net>
2013-12-18 22:34:21 +0100

    Now it works on files using stdin/stdout

--- iksoj.go
+++ iksoj.go
@@ -1,37 +1,60 @@
 package main
 
-import "fmt"
+//import "fmt"
 import "flag"
 import "strings"
+import "bufio"
+import "os"
+import "io"
 
-func konverti(de string, kien bool) string {
+func konverti(de string, iksen bool) string {
 	returnstring := de
 	a := 0
 	b := 1
-	if kien {
+	if iksen {
 		a, b = 1, 0
 	}
-	tabelo := [6][2]string{
+	tabelo := [18][2]string{
 		{"cx", "ĉ"},
 		{"gx", "ĝ"},
 		{"hx", "ĥ"},
 		{"jx", "ĵ"},
 		{"sx", "ŝ"},
 		{"ux", "ŭ"},
+		{"Cx", "Ĉ"},
+		{"Gx", "Ĝ"},
+		{"Hx", "Ĥ"},
+		{"Jx", "Ĵ"},
+		{"Sx", "Ŝ"},
+		{"Ux", "Ŭ"},
+		{"CX", "Ĉ"},
+		{"GX", "Ĝ"},
+		{"HX", "Ĥ"},
+		{"JX", "Ĵ"},
+		{"SX", "Ŝ"},
+		{"UX", "Ŭ"},
 	}
-	for i := 0; i<6; i++ {
-		returnstring = strings.Replace(returnstring, tabelo[i][a], tabelo[i][b], -1)
+	for i := 0; i<18; i++ {
+		returnstring = strings.Replace(returnstring,
+				tabelo[i][a], tabelo[i][b], -1)
 	}
 	return returnstring
 }
 
+func konvertifluon(fluo *bufio.Reader, kien io.Writer, iksen bool) {
+	for i, err:=fluo.ReadString('\n'); err == nil; i, err=fluo.ReadString('\n'){
+		io.WriteString(kien, konverti(i, iksen))
+	}
+}
+
 func main() {
 	var direkto *bool
-	var enigo string
+	//var enigo string
 
 	direkto = flag.Bool("iksen", false, "Traduki al iksoj")
 
 	flag.Parse()
-	enigo = strings.Join(flag.Args()," ")
-	fmt.Print(konverti(enigo,*direkto))
+	konvertifluon(bufio.NewReader(os.Stdin), os.Stdout, *direkto)
+	//enigo = strings.Join(flag.Args()," ")
+	//fmt.Print(konverti(enigo,*direkto))
 }
