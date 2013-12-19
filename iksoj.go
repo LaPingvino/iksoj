3878f64b72ac2889ddb0d02e1a69cb24688f8a1f
Joop Kiefte <joop@kiefte.net>
2013-12-19 19:51:43 +0100

    Clean up visual aspects: table uses less screen space and help text is bilingual now

--- iksoj.go
+++ iksoj.go
@@ -7,37 +7,25 @@ import "os"
 import "io"
 
 func konverti(de string, iksen bool) string {
-	returnstring := de
 	a := 0
 	b := 1
 	if iksen {
 		a, b = 1, 0
 	}
 	tabelo := [18][2]string{
-		{"cx", "ĉ"},
-		{"gx", "ĝ"},
-		{"hx", "ĥ"},
-		{"jx", "ĵ"},
-		{"sx", "ŝ"},
-		{"ux", "ŭ"},
-		{"Cx", "Ĉ"},
-		{"Gx", "Ĝ"},
-		{"Hx", "Ĥ"},
-		{"Jx", "Ĵ"},
-		{"Sx", "Ŝ"},
-		{"Ux", "Ŭ"},
-		{"CX", "Ĉ"},
-		{"GX", "Ĝ"},
-		{"HX", "Ĥ"},
-		{"JX", "Ĵ"},
-		{"SX", "Ŝ"},
-		{"UX", "Ŭ"},
+		{"cx", "ĉ"}, {"gx", "ĝ"}, {"hx", "ĥ"},
+		{"jx", "ĵ"}, {"sx", "ŝ"}, {"ux", "ŭ"},
+
+		{"Cx", "Ĉ"}, {"Gx", "Ĝ"}, {"Hx", "Ĥ"},
+		{"Jx", "Ĵ"}, {"Sx", "Ŝ"}, {"Ux", "Ŭ"},
+
+		{"CX", "Ĉ"}, {"GX", "Ĝ"}, {"HX", "Ĥ"},
+		{"JX", "Ĵ"}, {"SX", "Ŝ"}, {"UX", "Ŭ"},
 	}
 	for i := 0; i<18; i++ {
-		returnstring = strings.Replace(returnstring,
-				tabelo[i][a], tabelo[i][b], -1)
+		de = strings.Replace(de, tabelo[i][a], tabelo[i][b], -1)
 	}
-	return returnstring
+	return de
 }
 
 func konvertifluon(fluo *bufio.Reader, kien io.Writer, iksen bool) {
@@ -47,7 +35,7 @@ func konvertifluon(fluo *bufio.Reader, kien io.Writer, iksen bool) {
 }
 
 func main() {
-	direkto := flag.Bool("iksen", false, "Traduki al iksoj. Defaŭlte tradukiĝas al veraj ĉapeloj.")
+	direkto := flag.Bool("x", false, "Traduki al iksoj. Convert to x-system.)")
 	flag.Parse()
 
 	enigo := flag.Arg(0)
